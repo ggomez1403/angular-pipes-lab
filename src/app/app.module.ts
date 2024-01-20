c476c17ca4da6ccfe9318dd740468b4523cfb8b2
Geraldine Gómez <geralgomez1403@gmail.com>
2024-01-20 17:55:15 -0500

    Angular Pipes Lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CapitalTextPipe } from './capital-text.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalTextPipe
   ],
   imports: [
     BrowserModule
